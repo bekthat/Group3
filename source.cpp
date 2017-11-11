2549b168323fc64fd986155a43500d405351f357
muslimbeibytuly <muslimbeibytuly@gmail.com>
2017-11-11 10:29:38 +0600

    triangle

--- source.cpp
+++ source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 class Student {
@@ -55,13 +56,39 @@ class Rectangle {
     }
 };
 
-int main() {
+class Triangle {
+    int a, b, c;
+public:
+    void set(int _a, int _b, int _c){
+        if(_a + _b >= _c && _a + _c >= _b && _b + _c >= _a) {
+            a = _a;
+            b = _b;
+            c = _c;
+        }
+        else {
+            cout << "a, b and c are wrong values for triangle";
+        }
+    }
+    float getArea(){
+        float p = (a + b + c) / 2.0;
+        return sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+};
+void f1(){
     Student student1;
     student1.set();
     Student student2;
     student2.set();
 
+    student1.setGroup(2);
     student1.print();
     student2.print();
+}
+void f2() {
+    Triangle a;
+    a.set(3, 4, 5);
+    cout << a.getArea() << endl;
+}
+int main() {
     return 0;
 }
